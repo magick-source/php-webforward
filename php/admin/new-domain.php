ef469226639fe984037c356e99e485e5392c664e
theMage <themage@magick-source.net>
2023-07-23 18:03:56 +0200

    remove old, unused code
    themage

--- php/admin/new-domain.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!function_exists('check_login')) { include("include.php"); }
-
-$smarty->assign('forward', array('forward'=>'http://'));
-$smarty->assign('pagetitle', 'New Domain');
-$smarty->assign('template', 'edit-domain.tpl');
-$smarty->display('main.tpl');
-
-?>
